a433bffa68848f776349510ac18c43d7190166f1
NishadGuha <54943334+NishadGuha@users.noreply.github.com>
2021-04-24 03:04:20 +0200

    Updated UI Positioning and Trying Google Fonts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './vectors/coinmarketcap-1.svg';
+import { useEffect } from 'react'
+import logo from './vectors/coinmarketcap-1.svg'
 import './App.css';
 import Cryptos from './components/crypto/crypto'
+
 import Button from '@material-ui/core/Button'
 import PlayIcon from '@material-ui/icons/PlayCircleFilled'
 import TextField from '@material-ui/core/TextField'
 import { makeStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import WebFont from 'webfontloader'
+
+
+import AppBar from '@material-ui/core/AppBar'
+import ToolBar from '@material-ui/core/ToolBar'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
 
 const useStyles = makeStyles({
   root: {
@@ -12,7 +22,9 @@ const useStyles = makeStyles({
     border: 0,
     borderRadius: 10,
     color: 'white',
-    padding: '5px 15px'
+    padding: '5px 15px',
+    position: 'relative',
+    top: '-400px'
   }
 })
 
@@ -28,14 +40,35 @@ function ButtonStyled() {
 
 function App() {
 
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Montserrat']
+      }
+    });
+   }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <p className="powered-by">Powered by <img src={logo} className="App-logo" alt="logo"/>
+      <AppBar>
+        <ToolBar>
+          <IconButton>
+            <MenuIcon />
+          </IconButton>
+          <Typography className="app-title" variant="h6">
+            Cryptocurrency Profit Calculator
+          </Typography>
+          <Button className="about-button">
+            About
+          </Button>
+        </ToolBar>
+      </AppBar>
+        <p className="powered-by">Powered by <img src={logo} className="app-logo" alt="logo"/>
         </p>
         <Cryptos />
         <br/>
-        <p>If I invested $<TextField variant="outlined" color="primary" label="Amount in USD"/> in BTC on <TextField type="date" variant="outlined" color="primary"/>, then today I would have...</p>
+        <p className="tool">If I invested $<TextField variant="outlined" color="primary" label="Amount in USD"/> in BTC on <TextField type="date" variant="outlined" color="primary"/>, then today I would have...</p>
         <ButtonStyled />
       </header>
     </div>
--- src/components/crypto/crypto.js
+++ src/components/crypto/crypto.js
@@ -12,11 +12,16 @@ function Crypto() {
     }, []);
     
     return (
-        <div>
-        <h2>Top 10 Cryptocurrencies By Market Cap</h2>
+        <div className="main-div">
+        <h2>Top 10 Cryptocurrencies<br/>
+             By Market Cap</h2>
         <ul>
             {cryptos.map(crypto => 
-                <li key={crypto.id}>{crypto.cmc_rank}. <b>{crypto.name}</b> {crypto.quote.USD.price} 24h Change: {crypto.quote.USD.percent_change_24h}</li>
+                <li key={crypto.id}>
+                {crypto.cmc_rank}. <b>{crypto.name} </b> 
+                {Math.round(crypto.quote.USD.price*100)/100} <br/> 24h Change: 
+                <div className={(crypto.quote.USD.percent_change_24h < 0) ? "price-drop" : "price-moon"}>{crypto.quote.USD.percent_change_24h}</div>
+                </li>
             )}
         </ul>
         </div>
